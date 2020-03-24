2678f399010f2835587d75033f3d811aaa94d144
Sarang <sarang98divakaran@gmail.com>
2020-03-24 21:11:38 +0530

    completely enclose in try catch

--- src/controllers/inventory.js
+++ src/controllers/inventory.js
@@ -4,12 +4,13 @@ const Inventory = require('../helpers/inventory');
  * Add items to inventory
  */
 const addItem = async (data, req, res, next) => {
-    const item = req.body;
-
-    // Append store_id and filename to item.
-    item.store_id = req.params.store_id;
-    item.image_path = req._filename;
     try {
+        const item = req.body;
+
+        // Append store_id and filename to item.
+        item.store_id = req.params.store_id;
+        item.image_path = req._filename;
+
         // Add item to inventory
         await Inventory.addItem(item);
 
@@ -17,7 +18,6 @@ const addItem = async (data, req, res, next) => {
             status: 'success',
             message: 'Item added to inventory.'
         });
-
     } catch(err) {
         next(err);
     }
@@ -30,14 +30,13 @@ module.exports.addItem = addItem;
  * Edit an item in the inventory.
  */
 const editItem = async (data, req, res, next) => {
-
-    const item = req.body;
-
-    // Append item_id and filename to item.
-    item.item_id = req.params.item_id;
-    item.image_path = req._filename;
-
     try {
+        const item = req.body;
+
+        // Append item_id and filename to item.
+        item.item_id = req.params.item_id;
+        item.image_path = req._filename;
+
         // Edit item
         await Inventory.editItem(item);
 
@@ -45,11 +44,9 @@ const editItem = async (data, req, res, next) => {
             status: 'success',
             message: 'Item Edited.'
         });
-
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.editItem = editItem;
@@ -59,7 +56,6 @@ module.exports.editItem = editItem;
  * Edit only the image of an item in the inventory.
  */
 const editImage = async (data, req, res, next) => {
-
     try {
         // Append item_id and filename to item.
         const item = {
@@ -74,11 +70,9 @@ const editImage = async (data, req, res, next) => {
             status: 'success',
             message: 'Item image updated.'
         });
-        
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.editImage = editImage;
@@ -88,10 +82,8 @@ module.exports.editImage = editImage;
  * Delete an item from the inventory
  */
 const deleteItem = async (data, req, res, next) => {
-
-    const { item_id } = req.params;
-
     try {
+        const { item_id } = req.params;
 
         await Inventory.deleteItem(item_id);
 
@@ -99,11 +91,9 @@ const deleteItem = async (data, req, res, next) => {
             status: 'success',
             message: 'Item Deleted.'
         });
-
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.deleteItem = deleteItem;
@@ -113,11 +103,8 @@ module.exports.deleteItem = deleteItem;
  * Get details of a single item by its ID
  */
 const getItemById = async (req, res, next) => {
-
-    const { item_id } = req.params;
-
     try {
-
+        const { item_id } = req.params;
         const item = await Inventory.getItemById(item_id);
 
         res.json({
@@ -142,7 +129,7 @@ module.exports.getItemById = getItemById;
  *  - match category_name in category table.
  *  - match sub_category_name in sub category table.
  *  - match brand_name in brands table.
- * 
+ *
  * @param {Object} req.query - Query Parameters
  * @param {String} req.query.search - Search String
  * @param {Number} req.query.page - Page number
@@ -150,44 +137,36 @@ module.exports.getItemById = getItemById;
  * to be displayed in a page.
  */
 const search = async (req, res, next) => {
-
-    const page = parseInt(req.query.page) || 1;
-    const per_page = parseInt(req.query.per_page) || 10;
-
-    const offset = (page - 1 ) * per_page;
-
-    // Object to be passed to search helper.
-    const options = {
-        search: req.query.search ? req.query.search : null,
-        offset,
-        limit: per_page
-    };
-    
-    // Set scope of variable outside of try block.
-    let result;
-
     try {
+        const page = parseInt(req.query.page) || 1;
+        const per_page = parseInt(req.query.per_page) || 10;
 
-        result = await Inventory.searchItems(options);
+        const offset = (page - 1 ) * per_page;
 
+        // Object to be passed to search helper.
+        const options = {
+            search: req.query.search ? req.query.search : null,
+            offset,
+            limit: per_page
+        };
+
+        let result = await Inventory.searchItems(options);
+
+        // combine result with extra properties.
+        Object.assign(result, {
+            total_pages: Math.ceil(result.total/per_page),
+            per_page: per_page,
+            current_page: page,
+            base_url: process.env.IMAGE_BASE_URL
+        });
+
+        res.json({
+            status: 'success',
+            data: result
+        });
     } catch(err) {
         next(err);
     }
-
-    // combine result with extra properties.
-    Object.assign(result, {
-        total_pages: Math.ceil(result.total/per_page),
-        per_page: per_page,
-        current_page: page,
-        base_url: process.env.IMAGE_BASE_URL
-    });
-
-
-    res.json({
-        status: 'success',
-        data: result
-    });
-
 }
 
 module.exports.search = search;
@@ -197,9 +176,7 @@ module.exports.search = search;
  * Get list of all categories and their IDs.
  */
 const getCategories = async (req, res, next) => {
-    
     try {
-
         // Get categories
         const categories = await Inventory.getCategories();
 
@@ -210,12 +187,10 @@ const getCategories = async (req, res, next) => {
                 count: categories.length,
                 base_url: process.env.CATEGORY_URL
             }
-        })
-
+        });
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getCategories = getCategories;
@@ -225,11 +200,9 @@ module.exports.getCategories = getCategories;
  * Get list of all categories and their IDs.
  */
 const getSubCategories = async (req, res, next) => {
-    
     try {
-
         const { category_id } = req.params;
- 
+
         // Get sub categories
         const sub_categories = await Inventory.getSubCategories(category_id);
 
@@ -240,12 +213,10 @@ const getSubCategories = async (req, res, next) => {
                 count: sub_categories.length,
                 base_url: process.env.CATEGORY_URL
             }
-        })
-
+        });
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getSubCategories = getSubCategories;
@@ -255,11 +226,9 @@ module.exports.getSubCategories = getSubCategories;
  * Get list of all categories and their IDs.
  */
 const getSubSubCategories = async (req, res, next) => {
-    
     try {
-
         const { sub_category_id } = req.params;
- 
+
         // Get sub categories
         const sub_sub_categories = await Inventory.getSubSubCategories(sub_category_id);
 
@@ -269,13 +238,11 @@ const getSubSubCategories = async (req, res, next) => {
                 sub_sub_categories,
                 count: sub_sub_categories.length,
                 base_url: process.env.CATEGORY_URL
-            }
-        })
-
+            },
+        });
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getSubSubCategories = getSubSubCategories;
@@ -285,9 +252,7 @@ module.exports.getSubSubCategories = getSubSubCategories;
  * get all categories, sub categories and sub sub categories.
  */
 const getAllCategories = async (req, res, next) => {
-    
     try {
- 
         const categories = await Inventory.getAllCategories();
 
         res.json({
@@ -296,12 +261,10 @@ const getAllCategories = async (req, res, next) => {
                 categories,
                 base_url: process.env.CATEGORY_URL
             }
-        })
-
+        });
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getAllCategories = getAllCategories;
@@ -311,9 +274,7 @@ module.exports.getAllCategories = getAllCategories;
  * Get list of all brands and their IDs.
  */
 const getBrands = async (req, res, next) => {
-    
     try {
-
         // Get brands
         const brands = await Inventory.getBrands();
 
@@ -322,19 +283,17 @@ const getBrands = async (req, res, next) => {
             data: {
                 brands
             }
-        })
-
+        });
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getBrands = getBrands;
 
 
 /**
- * 
+ *
  * @param {Object} req.query - Query Parameters.
  * @param {String} req.query.category_id - Level 1 Category ID.
  * @param {Number} req.query.sub_category_id - Level 2 Category ID.
@@ -344,46 +303,39 @@ module.exports.getBrands = getBrands;
  * @param {String} req.query.stock - String true or false, true selects outofstock products
  */
 const getItemsByCategory = async (req, res, next) => {
-
-    const page = parseInt(req.query.page) || 1;
-    const per_page = parseInt(req.query.per_page) || 10;
-
-    const offset = ( page - 1 ) * per_page;
-
-    // Object to be passed to getItemsByCategory helper.
-    const options = {
-        category_id: req.query.category_id ? req.query.category_id : null,
-        sub_category_id: req.query.sub_category_id ? req.query.sub_category_id : null,
-        sub_sub_category_id: req.query.sub_sub_category_id ? req.query.sub_sub_category_id : null,
-        stock: req.query.stock === 'true'? true : false,
-        offset,
-        limit: per_page
-    };
-    
-    // Set scope of variable outside of try block.
-    let result;
-
     try {
+        const page = parseInt(req.query.page) || 1;
+        const per_page = parseInt(req.query.per_page) || 10;
 
-        result = await Inventory.getItemsByCategory(options);
+        const offset = ( page - 1 ) * per_page;
 
+        // Object to be passed to getItemsByCategory helper.
+        const options = {
+            category_id: req.query.category_id ? req.query.category_id : null,
+            sub_category_id: req.query.sub_category_id ? req.query.sub_category_id : null,
+            sub_sub_category_id: req.query.sub_sub_category_id ? req.query.sub_sub_category_id : null,
+            stock: req.query.stock === 'true'? true : false,
+            offset,
+            limit: per_page
+        };
+
+        let result = await Inventory.getItemsByCategory(options);
+
+        // combine result with extra properties.
+        Object.assign(result, {
+            total_pages: Math.ceil(result.total/per_page),
+            per_page: per_page,
+            current_page: page,
+            base_url: process.env.IMAGE_BASE_URL
+        });
+
+        res.json({
+            status: 'success',
+            data: result
+        });
     } catch(err) {
         next(err);
     }
-
-    // combine result with extra properties.
-    Object.assign(result, {
-        total_pages: Math.ceil(result.total/per_page),
-        per_page: per_page,
-        current_page: page,
-        base_url: process.env.IMAGE_BASE_URL
-    });
-
-    res.json({
-        status: 'success',
-        data: result
-    });
-
 }
 
 module.exports.getItemsByCategory = getItemsByCategory;
@@ -391,16 +343,14 @@ module.exports.getItemsByCategory = getItemsByCategory;
 
 /**
  * Get list of all items in the inventory.
- * 
+ *
  * @param {Object} req.query - Query Parameters.
  * @param {Number} req.query.page - Page number.
  * @param {Number} req.query.per_page - Number of items to be displayed in a page.
  * @param {String} req.query.stock - String true or false, true selects outofstock products
  */
 const getAllItems = async (req, res, next) => {
-
     try {
-
         const page = parseInt(req.query.page) || 1;
         const per_page = parseInt(req.query.per_page) || 10;
 
@@ -413,16 +363,7 @@ const getAllItems = async (req, res, next) => {
             limit: per_page
         };
 
-        // Set scope of variable outside of try block.
-        let result;
-
-        try {
-
-            result = await Inventory.getAllItems(options);
-
-        } catch(err) {
-            next(err);
-        }
+        let result = await Inventory.getAllItems(options);
 
         // combine result with extra properties.
         Object.assign(result, {
@@ -432,16 +373,13 @@ const getAllItems = async (req, res, next) => {
             base_url: process.env.IMAGE_BASE_URL
         });
 
-
         res.json({
             status: 'success',
             data: result
         });
-
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getAllItems = getAllItems;
@@ -449,14 +387,12 @@ module.exports.getAllItems = getAllItems;
 
 /**
  * Suggest items
- *  
+ *
  * @param {Object} req.query - Query Parameters
  * @param {String} req.query.search - Search String
  */
 const suggest = async (req, res, next) => {
-
     try {
-
         const search = req.query.search;
 
         const result = await Inventory.suggestItems(search);
@@ -465,11 +401,9 @@ const suggest = async (req, res, next) => {
             status: 'success',
             data: result
         });
-
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.suggest = suggest;
@@ -477,18 +411,16 @@ module.exports.suggest = suggest;
 
 /**
  * Get Items that has some offer applied.
- * 
+ *
  * @param {Object} req.params - Query Parameters
  * @param {Number} req.params.offer_id - Offer ID.
  * @param {Number} req.params.page - Page Number.
  * @param {Number} req.params.per_page - Number of items to be displayed per page.
  */
 const getItemsByOfferID = async (req, res, next) => {
-
     try {
-
         const offer_id = req.query.offer_id;
-        
+
         const page = parseInt(req.query.page) || 1;
         const per_page = parseInt(req.query.per_page) || 100;
 
@@ -512,11 +444,9 @@ const getItemsByOfferID = async (req, res, next) => {
             status: 'success',
             data: items
         });
-
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getItemsByOfferID = getItemsByOfferID;
@@ -524,15 +454,13 @@ module.exports.getItemsByOfferID = getItemsByOfferID;
 
 /**
  * Get random list of items in the inventory.
- * 
+ *
  * @param {Object} req.query - Query Parameters.
  * @param {Number} req.query.page - Page number.
  * @param {Number} req.query.per_page - Number of items to be displayed in a page.
  */
 const getRandomItems = async (req, res, next) => {
-
     try {
-
         const page = parseInt(req.query.page) || 1;
         const per_page = parseInt(req.query.per_page) || 20;
 
@@ -558,11 +486,9 @@ const getRandomItems = async (req, res, next) => {
             status: 'success',
             data: result
         });
-
     } catch(err) {
         next(err);
     }
-
 }
 
 module.exports.getRandomItems = getRandomItems;
