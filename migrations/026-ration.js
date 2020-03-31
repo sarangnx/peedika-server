a0aacfc60eb5b89bc58c2a898e92e0f1850de9e1
Sarang <sarang98divakaran@gmail.com>
2020-03-31 21:39:25 +0530

    Added seeders, model and migration for ration

---
+++ migrations/026-ration.js
@@ -0,0 +1,71 @@
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('ration', {
+            entry_id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'users',
+                    key: 'user_id'
+                }
+            },
+            name: {
+                type: Sequelize.STRING(255),
+                allowNull: true
+            },
+            card_no: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+            aadhar_no: {
+                type: Sequelize.STRING(30),
+                allowNull: true,
+            },
+            phone: {
+                type: Sequelize.STRING(15),
+                allowNull: true,
+            },
+            quantity: {
+                type: Sequelize.DECIMAL(8,3),
+                allowNull: true,
+                defaultValue: 1
+            },
+            unit: {
+                type: Sequelize.ENUM,
+                values: [
+                    'g',
+                    'kg',
+                    'ml',
+                    'l',
+                    'count'
+                ],
+                allowNull: false,
+                defaultValue: 'kg'
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('NOW')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('NOW')
+            },
+            deletedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('ration');
+    }
+};
---
+++ src/models/ration.js
@@ -0,0 +1,65 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const ration = sequelize.define('ration', {
+        entry_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        user_id: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        card_no: {
+            type: DataTypes.STRING(30),
+            allowNull: true,
+        },
+        aadhar_no: {
+            type: DataTypes.STRING(30),
+            allowNull: true,
+        },
+        phone: {
+            type: DataTypes.STRING(15),
+            allowNull: true,
+        },
+        quantity: {
+            // defines a unit of the item.
+            type: DataTypes.DECIMAL(8,3),
+            allowNull: true,
+            defaultValue: 1
+        },
+        unit: {
+            // Base unit for defining quantity.
+            type: DataTypes.ENUM,
+            values: [
+                'g',
+                'kg',
+                'ml',
+                'l',
+                'count'
+            ],
+            allowNull: false,
+            defaultValue: 'kg'
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.fn('NOW'),
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.fn('NOW'),
+        }
+    },{ freezeTableName: true, paranoid:true });
+
+    ration.associate = function(models) {
+        ration.belongsTo(models.users, {
+            foreignKey: 'user_id',
+        });
+    };
+    return ration;
+};
